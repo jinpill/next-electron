7de6597f64d374bb120b3559f844531ca7a4b3bf
jinpill <hello@jinpill.dev>
2023-09-02 16:44:19 +0900

    👷 Change build configuration

--- .builder.config.js
+++ .builder.config.js
@@ -1,24 +1,41 @@
-/**
- * @type {import('electron-builder').Configuration)}}
- */
-module.exports = {
-  appId: "my.electron.app",
-  productName: "My App",
-  directories: {
-    buildResources: "buildSrc",
-    output: "build",
-  },
-  files: [
-    "dist/**/*",
-    "src/renderer/build/**/*",
-    "src/renderer/out/**/*",
-    ".staging-vars.json",
-    "package.json",
-  ],
-  win: {
-    target: "nsis",
-  },
-  mac: {
-    target: "dmg",
-  },
+// ref: https://www.electron.build/configuration/configuration
+
+module.exports = async (env) => {
+  const compression = env.isStaging ? "store" : "maximum";
+
+  /**
+   * @type {import('electron-builder').Configuration)}}
+   */
+  const config = {
+    appId: "my.electron.app",
+    productName: "My App",
+    artifactName: "${name}-${version}-${arch}.${ext}",
+    directories: {
+      buildResources: "buildSrc",
+      output: "build",
+    },
+    asar: true,
+    compression: compression,
+    files: [
+      "dist/**/*",
+      "src/renderer/build/**/*",
+      "src/renderer/out/**/*",
+      ".staging-vars.json",
+      "package.json",
+    ],
+    win: {
+      target: "nsis",
+    },
+    nsis: {
+      uninstallDisplayName: "${productName} ${version}",
+    },
+    mac: {
+      target: "dmg",
+    },
+    dmg: {
+      title: "${productName} ${version}",
+    },
+  };
+
+  return config;
 };
