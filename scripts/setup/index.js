b086b25cb9ddfaa2bd9757b79137df63e45866d0
jinpill <jinlee@metamorp.cc>
2024-01-30 22:37:18 +0900

    🔨 Add version setup to the setup script

--- scripts/setup/index.js
+++ scripts/setup/index.js
@@ -7,6 +7,7 @@ const packageJson = require("../utils/packageJson");
 packageJson.update((json) => {
   json.name = env.config.project;
   json.scripts["dev:render"] = `next dev src/renderer -p ${env.config.port}`;
+  json.version = "1.0.0";
   return json;
 });
 console.log(clc.green("✅ Setup Success!"));
