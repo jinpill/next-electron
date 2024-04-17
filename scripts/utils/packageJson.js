2cdbd1f8f2aca241959ea739f182201f06248fbe
jinpill <jinlee@metamorp.cc>
2024-04-17 16:13:08 +0900

    🐛 Fix a bug that changes the package.json file on Windows

--- scripts/utils/packageJson.js
+++ scripts/utils/packageJson.js
@@ -1,5 +1,6 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
+const os = require("node:os");
 
 const env = require("./env");
 const jsonPath = path.resolve(env.__root, "./package.json");
@@ -14,11 +15,14 @@ const update = (callback) => {
   const nextJson = callback(json);
   if (typeof nextJson !== "object") return;
 
-  const contents = JSON.stringify(nextJson, null, 2) + "\n";
+  let contents = JSON.stringify(nextJson, null, 2) + "\n";
+  contents = contents.replace(/\n/g, os.EOL);
   fs.writeFileSync(jsonPath, contents, "utf8");
 };
 
-module.exports = {
+const utility = {
   get,
   update,
 };
+
+module.exports = utility;
